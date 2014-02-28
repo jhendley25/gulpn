756109a5c8f580b24634607f33f2af7a559c2ad0
jhendley25 <jake.hendley25@gmail.com>
2014-02-28 13:31:55 -0500

    more gulpn, livereload added

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
 var gulp = require('gulp');
 
 var coffee = require('gulp-coffee');
+var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var paths = {
-  scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
+  scripts: ['*.coffee', '!client/external/**/*.coffee'],
   images: 'client/img/**/*'
 };
 
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   return gulp.src(paths.scripts)
+    .pipe(watch())
     .pipe(coffee())
     .pipe(uglify())
     .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('build/js'));
-});
-
-// Rerun the task when a file changes
-gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['scripts']);
+    .pipe(gulp.dest('build/js'))
+    .pipe(livereload());
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts']);
