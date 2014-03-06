7526b20441d18a73fce7d120406ecbafe6725c24
jhendley25 <jake.hendley25@gmail.com>
2014-03-06 15:05:01 -0500

    working on livereload

--- gulpfilebackup.js
+++ gulpfilebackup.js
@@ -1,4 +1,3 @@
-// The default task (called when you run `gulp` from cli)
 var gulp = require('gulp');
 
 var coffee = require('gulp-coffee');
@@ -6,7 +5,25 @@ var livereload = require('gulp-livereload');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-gulp.task('default', ['watch']);
+
+var paths = {
+  scripts: ['*.coffee', '!client/external/**/*.coffee'],
+  images: 'client/img/**/*'
+};
+
+gulp.task('scripts', function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  return gulp.src(paths.scripts)
+    .pipe(watch())
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('build/js'))
+    .pipe(livereload());
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['scripts', 'watch']);
 
 var livereload = require('gulp-livereload'),
     dest = 'build';
